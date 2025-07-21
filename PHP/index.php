d6c1298d40bc344fe9c7a5a8fec739a1bee94020
LiveConnect pruebas <crizz@utp.edu.co>
2025-07-21 17:30:46 -0500

    Update index.php

---
+++ PHP/index.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <title>My First PHP Page</title>
+    </head>
+    <body>
+    <?php   
+        echo "Hello World!";
+    ?>
+    </body>
+</html>
+
+<!-- ejemplo 2 -->
+
+<html>
+  <head>
+    <title>My First PHP Page</title>
+  </head>
+  <body>
+  <script language="php">
+    echo "Hello World!";
+  </script>
+  </body>
+</html>
+
+
